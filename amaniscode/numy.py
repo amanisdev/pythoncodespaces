4226cb98a7a9eaeca84eb762d82d8c0f40119132
amanisdev <133533373+amanisdev@users.noreply.github.com>
2023-06-11 23:32:34 +0000

    minus

--- amaniscode/numy.py
+++ amaniscode/numy.py
@@ -7,8 +7,8 @@ numdiv = num1 / num2
 
 numplus = num1 + num2
 
-
-
+nummin = num1 - num2
+print ('minus: ' + str(nummin) )
 print ('add: ' + str(numplus))
 print('multiply: ' + str(numtimes))
 print ( 'divide: ' + str(numdiv))
